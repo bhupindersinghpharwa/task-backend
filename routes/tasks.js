96c8b77a1f066dfeb4daaa6b3ed47c46ca20e9a1
BhupindersinghPharwa <bhupindersingh.pharws@vernost.com>
2026-03-26 22:18:22 +0530

    Changes done for error handling

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const { createTask, listTasks, updateTask, deleteTask } = require('../controllers/tasksController');
 const { taskSchema, validate } = require('../middlewares/validate');
 
-router.post('/:project_id/tasks', validate(taskSchema), createTask);
-router.get('/:project_id/tasks', listTasks);
+router.post('/projects/:project_id/tasks', validate(taskSchema), createTask);
+router.get('/projects/:project_id/tasks', listTasks);
 
-router.put('/:id', validate(taskSchema), updateTask);
-router.delete('/:id', deleteTask);
+router.put('/tasks/:id', validate(taskSchema), updateTask);
+router.delete('/tasks/:id', deleteTask);
 
 module.exports = router;
